d67640c471b5b06d7d400c4d9eed9867c2ea1ea2
Sameer Chauhan <sameerchauhan212204@gmail.com>
2025-04-13 20:10:02 +0530

    UI added

--- Meblog/settings.py
+++ Meblog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogg'
+    'blogg',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
